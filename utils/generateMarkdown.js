724cb162e1a97cd60c21c441d2e0f3431e3388c0
Kyle Schoppmann <klparuu@yahoo.com>
2023-01-15 14:53:26 -0700

    Added generateMD functionality

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,40 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${data.description}
 
+## Table of Contents
+---
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+---
+${data.installation}
+
+## Usage
+---
+${data.usage}
+
+## License
+---
+${data.license}
+
+## Contributing
+---
+${data.contributing}
+
+## Tests
+---
+${data.tests}
+
+## Questions
+---
+${data.questions}
 `;
 }
 
